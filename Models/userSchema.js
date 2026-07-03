fbb3d7eb618717a3c847046a129ebbfde6a1a720
premasanub <premakumaran15@gmail.com>
2026-07-03 23:03:47 +0530

    user registration and login functionality with JWT authentication completed

---
+++ Models/userSchema.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema=mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true
+        },
+        password:{
+            type:String,
+            required:true
+        },
+        token:{
+            type:String
+        },
+        role:{
+            type:String,
+            default:"user",
+            enum:["user","admin"]
+        }
+
+    },
+  
+
+)
+const User=mongoose.model("User",userSchema)
+export default User;
--- index.js
+++ index.js
@@ -2,22 +2,38 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDb from "./DataBase/dbConfig.js";
+import authRoutes from "./Routes/authRoutes.js";
 
-
+//dotenv config
 dotenv.config();
 
-const app = express();
+
+//express initialization
+const app=express();
+
+
+
+//default middleware
 app.use(express.json());
 app.use(cors());
 
+
+
+//Database connection
 connectDb();
 
-app.get("/", (req, res) => {
-    res.send("welcome to backend");
-})
+//default route
+app.get("/",(req,res) =>{
+    res.status(200).send({message:"welcome to Fitness Class backend"});
+});
 
-const port = process.env.PORT || 5000;
+
+//port
+const port=process.env.PORT || 5000;
+
+//custom routes
+app.use("/api/auth",authRoutes);
 
 app.listen(port, () => {
     console.log("Server Started");
-})
+});
