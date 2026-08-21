ba2f131c6d54e22f8896005c75d21b1b0391c125
premasanub <premakumaran15@gmail.com>
2026-08-21 17:05:38 +0530

    updated

--- Utils/mailer.js
+++ Utils/mailer.js
@@ -3,38 +3,38 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
 
- const sendMail = async (to, subject, text) => {
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-   tls: {
-     rejectUnauthorized: false
-   },
-    port: 465,
-    secure: true,
-    logger: true,
-    debug: true,
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_KEY
-    }
-  });
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_KEY,
+  },
 
-  const mailData = {
-    from: process.env.PASS_MAIL,
-    to,
-    subject,
-    text,
-  };
-  await new Promise((resolve, reject) => {
-    transporter.sendMail(mailData, (err, info) => {
-      if (err) {
-        reject(err);
-      } else {
-        console.log("Mail Send Successfully")
-        resolve(info);
-      }
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 15000,
+});
+
+const sendMail = async (to, subject, text) => {
+  try {
+    console.log("Sending email to:", to);
+
+    await transporter.sendMail({
+      from: process.env.MAIL_USER,
+      to,
+      subject,
+      text,
     });
-  });
+
+    console.log("Mail sent successfully");
+
+  } catch (error) {
+    console.error("Mail sending error:", error);
+    throw error;
+  }
 };
+
 export default sendMail;
